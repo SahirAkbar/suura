f91ca578590cad02f3c56e095614c5c5f58a4703
sahir <sahiritoo2015@gmail.com>
2023-11-09 00:27:43 +0530

    added middleware and schema

---
+++ backend/MiddleWare/AuthenticateSchema.js
@@ -0,0 +1,36 @@
+const {generateErrorMessage} = require('zod-error')
+const options  = {
+  delimiter: {
+    error: " error ",
+  },
+  code: {
+    enabled: false,
+  },
+  path: {
+    enabled: false,
+  },
+  maxErrors: 1,
+  transform: ({ errorMessage, index }) => `  ${errorMessage}`,
+};
+const validate =
+  (schema) =>
+  (req, res, next) => {
+    console.log("Called",schema);
+    try {
+      const result = schema.safeParse({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+      if (!result.success) {
+        const errorMessage = generateErrorMessage(result.error.issues, options);
+        console.log("errorMessage", errorMessage);
+        return res.status(400).send(errorMessage);
+      }
+      next();
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ message: error });
+    }
+  };
+module.exports = validate;
