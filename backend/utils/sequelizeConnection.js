1b316e0358a93a1d9f9b1e046d42e73067a16cf8
Majidkhanliver <62938113+Majidkhanliver@users.noreply.github.com>
2023-11-18 21:55:30 +0530

    Packagae.json updated

--- backend/utils/sequelizeConnection.js
+++ backend/utils/sequelizeConnection.js
@@ -4,6 +4,6 @@ const tedious = require("tedious");
 const sequelize = new Sequelize("suura", "root", "root", {
   host: "localhost",
   dialect: "mysql",
-  port:4306
+  port:3306
 });
 module.exports = sequelize;
