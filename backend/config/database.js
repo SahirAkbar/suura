fb0f796870413597e720d625f8895d359fb7ed17
Majidkhanliver <62938113+Majidkhanliver@users.noreply.github.com>
2023-11-20 20:09:32 +0530

    some config changed

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 const db = mysql.createConnection({
   // host: 'mysqldb',
-  host:'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'suura',
-  port: 4306
+  host: "mysqldb",
+  user: "root",
+  password: "root",
+  database: "suura",
+  port: 3306,
 });
 
